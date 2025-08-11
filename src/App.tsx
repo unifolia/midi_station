65f4654f2746f5f113e2d4d8ecde9452abdfa64c
James <james@jameslewis.io>
2025-08-10 21:33:04 -0400

    Readded callback hook

--- src/App.tsx
+++ src/App.tsx
@@ -163,7 +163,7 @@ const App = () => {
     }
   };
 
-  const handleMIDIMessage = (event: any) => {
+  const handleMIDIMessage = useCallback((event: any) => {
     // Currently must be same MIDI device send/receive, but remove this to change
     if (event.target.name === deviceRef.current) {
       const [status, data1, data2] = event.data;
@@ -190,7 +190,7 @@ const App = () => {
         }
       }
     }
-  };
+  }, []);
 
   useEffect(() => {
     if (navigator.requestMIDIAccess) {
